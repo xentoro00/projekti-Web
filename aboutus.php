276dba318b897224d1c67687175d0d9765a3882f
xentoro00 <diorhyseni0@gmail.com>
2024-01-29 00:56:13 +0100

    updated

---
+++ aboutus.php
@@ -0,0 +1,54 @@
+<?php 
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+
+
+.container {
+  width: 80%;
+  margin: auto;
+  background-color: #fff;
+  box-shadow: 0px 0px 20px rgba(0,0,0,0.1);
+  border-radius: 10px;
+  padding: 20px;
+  margin-top: 80px;
+}
+
+.about-us {
+  background-color: #f9f9f9;
+  padding: 20px;
+  margin-top: 20px;
+  border-radius: 10px;
+}
+
+.about-us h2 {
+  color: #333;
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+}
+
+.about-us p {
+  color: #666;
+  line-height: 1.6;
+  text-align: justify;
+}
+</style>
+</head>
+<body>
+<?php include 'header.php'; ?>
+
+<div class="container">
+  <div class="about-us">
+    <h2>Rreth Nesh</h2>
+    <p>Dyqani juaj i vetëm për kompjuterë dhe laptopë. Tek Dyqani Online, ne jemi krenarë që ofrojmë një gamë të gjerë të kompjuterëve dhe laptopëve për të përmbushur të gjitha nevojat tuaja. Përkushtimi ynë ndaj cilësisë dhe përballueshmërisë siguron se do të gjeni produktin e përsosur në çmimin e duhur. Bëni pazar me ne sot dhe përjetoni lehtësinë e blerjeve online.</p>
+  </div>
+</div>
+
+</body>
+
+</html>
---
+++ shop.php
@@ -0,0 +1,67 @@
+<?php 
+  session_start();
+?>
+<html>
+    <head>
+        <title>helo</title>
+        <link rel="stylesheet" href="shop.css?v=<?php echo time(); ?>">
+
+    </head>
+    <body>
+    <?php include 'header.php'; ?>
+    <?php include './includes/config.php'; ?>
+
+    <main>
+        <div class="product-slider">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                     <?php
+                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                            $id = $_POST['id'];
+                            $uID = $_SESSION['uID'];
+                            echo $id . "<br>" . $uID;
+                            $sql = "INSERT INTO checkout (productID, userID) VALUES ('$id', '$uID')";
+
+                            if ($con->query($sql) !== TRUE) {
+                                echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+                        }
+                    if (isset($_GET["categoryID"])) {
+                        $cID = $_GET["categoryID"];
+                        $select = mysqli_query($con, "SELECT * FROM products WHERE KategoriID= '$cID '");
+                    }else {
+                     $select = mysqli_query($con, "SELECT * FROM products");
+                }
+                      while($row = mysqli_fetch_assoc($select)){ 
+                     ?>
+                   
+                <div class="swiper-slide">
+                    <form action="" method="post">
+                        <div class="image-container">
+                            <img src="data:image/jpeg;base64,<?php echo $row['Foto']; ?>" height="100" alt="Product <?php echo $row['ProduktID']; ?>">
+                        </div>
+                        <div class="title-container">
+                            <h3><?php echo $row['Emri']; ?></h3>
+                        </div>
+                        <div class="description-container">
+                            <?php echo $row['Pershkrimi']; ?>
+                        </div>
+                        <div class="price-container">
+                            <h2><?php echo $row['Cmimi']; ?>€</h2>
+                        </div>
+                        <div class="addToCart-container">
+                            <input type="hidden" name="id" value="<?php echo $row['ProduktID'] ?>">
+                            <button type="submit">Add to Cart</button>
+                        </div>
+                    </form>
+                </div> 
+                <?php } ?>
+                </div>
+            </div>
+        </div>
+        
+    </main>
+    <?php include 'footer.php'; ?>
+
+    </body>
+</html>
